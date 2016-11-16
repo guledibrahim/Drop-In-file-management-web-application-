1e776769840773aa8f33d796a73b575e472d688d
Guled Ibrahim <guledibrahim@hotmail.co.uk>
2016-11-16 13:23:33 +0000

    Update connectdp.php

--- connectdp.php
+++ connectdp.php
@@ -1,3 +1,3 @@
 <?php
- $mysqli=mysqli_connect('mysql13.000webhost.com','a5842631_234','west4life', 'a5842631_234') or die("Database Error");
+ $mysqli=mysqli_connect('mysql13.000webhost.com','a5842631_234','password', 'a5842631_234') or die("Database Error");
 ?>
